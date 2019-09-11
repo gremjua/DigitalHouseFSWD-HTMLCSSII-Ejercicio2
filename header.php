6bb35f86ed4894fd5091798c8ca8460c951a2db7
gremjua <gremjua@gmail.com>
2019-09-11 11:10:56 -0300

    Changed html to php, separated header and footer from home.

---
+++ header.php
@@ -0,0 +1,28 @@
+<header>
+            <div id="logo">
+                <img src="img/logo.png" alt="Craftsy Logo">
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li>
+                        <a href="null">SMARTPHONES</a>
+                    </li>
+                    <li>
+                        <a href="null">TABLETS</a>
+                    </li>
+                    <li>
+                        <a href="null">COMPUTADORAS</a>
+                    </li>
+                </ul>
+            </nav>
+            <div id="login" class="navigation">
+                <ul>
+                    <li>
+                        <a href="null">LOGIN</a>
+                    </li>
+                    <li>
+                        <a href="null">REGISTRO</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
--- index.php
+++ index.php
@@ -5,34 +5,9 @@
         <title>Home</title>
     </head>
     <body>
-        <header>
-            <div id="logo">
-                <img src="img/logo.png" alt="Craftsy Logo">
-            </div>
-            <nav class="navigation">
-                <ul>
-                    <li>
-                        <a href="null">SMARTPHONES</a>
-                    </li>
-                    <li>
-                        <a href="null">TABLETS</a>
-                    </li>
-                    <li>
-                        <a href="null">COMPUTADORAS</a>
-                    </li>
-                </ul>
-            </nav>
-            <div id="login" class="navigation">
-                <ul>
-                    <li>
-                        <a href="null">LOGIN</a>
-                    </li>
-                    <li>
-                        <a href="null">REGISTRO</a>
-                    </li>
-                </ul>
-            </div>
-        </header>
+        <?php
+            include_once('header.php');
+        ?>
         <main>
             <section id="productos">
                 <div class="productCard">
@@ -154,8 +129,8 @@
                 </div>
             </section>
         </main>
-        <footer>
-
-        </footer>
+        <?php
+            include_once('footer.php');
+        ?>
     </body>
 </html>
