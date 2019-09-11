1324d9fe141973a12eddfb00c0f9ee7542c6fc14
gremjua <gremjua@gmail.com>
2019-09-11 12:09:27 -0300

    Added discount field to product array.

--- index.php
+++ index.php
@@ -1,3 +1,57 @@
+<?php
+    $productos = [
+        ['img' => ['src' => 'img/img-phone-01.jpg', 'alt' => 'Imagen Celular 01'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 125.99,
+        'estaEnOferta' => true],
+        ['img' => ['src' => 'img/img-phone-02.jpg', 'alt' => 'Imagen Celular 02'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 12.50,
+        'estaEnOferta' => false],
+        ['img' => ['src' => 'img/img-phone-03.jpg', 'alt' => 'Imagen Celular 03'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 0.99,
+        'estaEnOferta' => true],
+        ['img' => ['src' => 'img/img-phone-01.jpg', 'alt' => 'Imagen Celular 01'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 125.99,
+        'estaEnOferta' => true],
+        ['img' => ['src' => 'img/img-phone-02.jpg', 'alt' => 'Imagen Celular 02'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 1250.99,
+        'estaEnOferta' => false],
+        ['img' => ['src' => 'img/img-phone-03.jpg', 'alt' => 'Imagen Celular 03'],
+        'nombre' => 'Nombre del producto',
+        'descripcion' => 'Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et.',
+        'link' => 'null',
+        'precio' => 125.00,
+        'estaEnOferta' => true],
+    ];
+
+    function descripcionLarga(array $producto){
+        $textoPrecio = '$' . sprintf('%.2f', $producto['precio']);
+        if($producto['estaEnOferta']){
+            $textoPrecioActual = '$' . sprintf('%.2f', 0.8*$producto['precio']);
+            $textoPrecio = strikeText($textoPrecio) . ' ' . $textoPrecioActual;
+        }
+        return $textoPrecio. '<br>' . $producto['descripcion'];
+    }
+
+    function strikeText(string $s) {
+        return '<strike>' . $s . '</strike>';
+    }
+?>
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -10,83 +64,25 @@
         ?>
         <main>
             <section id="productos">
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-01.jpg" alt="Imagen Celular 01">
+                <?php foreach($productos as $producto) : ?>
+                    <div class="productCard">
+                        <div>
+                            <img src="<?=$producto['img']['src']?>" alt="<?=$producto['img']['alt']?>">
+                        </div>
+                        <h2>
+                            <?=$producto['nombre']?>
+                        </h2>
+                        <!-- <h3>
+                            <?='$'.$producto['precio']?>
+                        </h3> -->
+                        <p>
+                            <!-- <?=$producto['descripcion']?> -->
+                            <?=descripcionLarga($producto)?>
+                        </p>
+                        <a href="<?=$producto['link']?>">VER MÁS</a>
                     </div>
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Eiusmod Lorem dolor labore eiusmod tempor amet ea nulla aliqua aliquip exercitation do reprehenderit cupidatat. Ut ad culpa et nisi est ullamco Lorem elit reprehenderit pariatur Lorem do incididunt et. 
-                    </p>
-                    <a href="null">VER MÁS</a>
-                </div>
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-02.jpg" alt="Imagen Celular 02">
-                    </div>
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Nostrud esse consequat amet non adipisicing. Nisi velit reprehenderit velit quis proident duis velit culpa excepteur amet anim nostrud et nostrud. Eu sint velit nulla laboris velit ex enim tempor elit velit laborum magna. Tempor magna anim irure eu dolor occaecat velit duis enim magna ut dolore. Exercitation enim laboris fugiat pariatur nulla nisi cupidatat et.
-                    </p>
-                    <a href="null">VER MÁS</a>
-
-                </div>
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-03.jpg" alt="Imagen Celular 03">
-                    </div>
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Aliquip consequat proident aute reprehenderit aliqua Lorem reprehenderit consequat. Cupidatat duis esse consectetur aute ea ipsum. Eu consectetur nulla ex voluptate officia amet aliquip est quis adipisicing consectetur enim.
-                    </p>
-                    <a href="null">VER MÁS</a>
-
-                </div>
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-01.jpg" alt="Imagen Celular 01">
-                    </div>
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Dolor esse fugiat adipisicing proident nisi excepteur aliqua. Proident amet consequat adipisicing esse. Nulla non sint ipsum cillum velit do nisi nisi commodo id duis deserunt aute. Labore commodo non nisi non quis dolor elit labore non.
-                    </p>
-                    <a href="null">VER MÁS</a>
-
-                </div>
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-02.jpg" alt="Imagen Celular 02">
-                    </div>   
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Esse non amet eiusmod ullamco minim Lorem consectetur occaecat id. Tempor deserunt eu nulla commodo eiusmod eu reprehenderit. Eiusmod proident incididunt fugiat est.
-                    </p>
-                    <a href="null">VER MÁS</a>
-
-                    </div>
-                <div class="productCard">
-                    <div>
-                        <img src="img/img-phone-03.jpg" alt="Imagen Celular 03">
-                    </div>
-                    <h2>
-                        Nombre del producto
-                    </h2>
-                    <p>
-                        Magna adipisicing deserunt veniam dolore ea laboris dolore ad ea Lorem. Consequat aute et ut id labore adipisicing exercitation. Deserunt qui irure ut mollit nulla eu dolor laborum nisi consectetur.
-                    </p>
-                    <a href="null">VER MÁS</a>
-
-                </div>
+                <?php endforeach;?>
+                
             </section>
             <section id="tutoriales" class="sideBar">
                 <h2>Tutoriales</h2>
